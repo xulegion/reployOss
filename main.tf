0542437d9c3626bcbad12172523c60383ac8c2c9
xulegion <73058380+xulegion@users.noreply.github.com>
2021-10-09 12:42:28 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
-resource "alicloud_oss_bucket" "bucket-acl-website" {
-  bucket = "bucket-010902-xnx"
-  acl    = "private"
+resource "alicloud_oss_bucket" "acl-website" {
+  bucket = "bucket-010902-xxx"
+  acl    = "public-read"
   
   website {
     index_document = "index.html"
     error_document = "error.html"
   }
+}
+
+
+resource "alicloud_oss_bucket_object" "object-content" {
+  bucket  = alicloud_oss_bucket.acl-website.bucket
+  key     = "companyName"
+  content = "Yunji."
 }
