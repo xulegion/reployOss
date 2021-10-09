f0e8580473e82d356a96db82a2a90d75a550fb57
xulegion <73058380+xulegion@users.noreply.github.com>
2021-10-09 11:43:53 +0800

    Update main.tf
    oss-acl-website

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resource "alicloud_oss_bucket" "bucket-acl" {
+resource "alicloud_oss_bucket" "bucket-acl-website" {
   bucket = "bucket-010902-nxx"
   acl    = "private"
+  
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
 }
